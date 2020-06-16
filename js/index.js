03f085c05b2f294af0c0aacf85448c11301259a4
Divyansh.Methi <Divyansh_Methi@dell.com>
2020-06-16 23:03:43 +0530

    Added adding number using expression and keyboard inputs

--- js/index.js
+++ js/index.js
@@ -1,55 +1,28 @@
-var operation = "";
-var equalResult = false;
-var firstOperand;
+var equalResult = true;
+var exp = "";
 
-// function myKeyPress(e){
-//     let keynum;
-//
-//     if(window.event) {
-//       keynum = e.keyCode;
-//     } else if(e.which){
-//       keynum = e.which;
-//     }
-//     let arrNum = ["zero","one","two","three","four","five","six","seven","eight","nine"];
-//     let arrChar = ["multiply","plus","","minus","","divide"];
-//     if(keynum <= 48 && keynum >= 57)
-//     {
-//       readNumber(arrNum[keynum-48]);
-//     }
-//     else if(keynum==61){
-//       performOperation();
-//     }
-//     else if(keynum==42 || keynum==43 || keynum==45 || keynum==47){
-//       readChar(arrChar[keynum-42]);
-//     }
-//     else {
-//     }
-//   }
+function keyPress(e)
+{
+  if(equalResult==false)
+  {
+    document.getElementById("inputValue").value = "";
+    equalResult = true;
+  }
+  else
+  {
+    let code = (e.keyCode ? e.keyCode : e.which);
+    if(code == 13) { //Enter keycode
+      performOperation();
+    }
+  }
+}
 
 function performOperation(){
-  let secondOperand = parseInt(document.getElementById("inputValue").value);
-  console.log(firstOperand);
-  console.log(secondOperand);
-  let res = "";
-  switch (operation) {
-      case "plus":
-        res += firstOperand + secondOperand;
-        break;
-      case "minus":
-        res += firstOperand - secondOperand;
-        break;
-      case "multiply":
-        res += firstOperand * secondOperand;
-        break;
-      case "divide":
-        res += firstOperand / secondOperand;
-        break;
-      default:
-        res = "0";
-        break;
-    }
-  document.getElementById("inputValue").value = res;
-  equalResult = true;
+  exp = document.getElementById("inputValue").value;
+  let expVal = eval(exp);
+  document.getElementById("inputValue").value = expVal;
+  exp = expVal;
+  equalResult = false;
 }
 
 function clearNumber()
@@ -68,59 +41,18 @@ function clearNumber()
 }
 
 function readNumber(eleName){
-    let inputValue = document.getElementById("inputValue").value;
-    let num;
-    switch (eleName) {
-      case "one":
-        num = 1;
-        break;
-      case "two":
-        num = 2;
-        break;
-      case "three":
-        num = 3;
-        break;
-      case "four":
-        num = 4;
-        break;
-      case "five":
-        num = 5;
-        break;
-      case "six":
-        num = 6;
-        break;
-      case "seven":
-        num = 7;
-        break;
-      case "eight":
-        num = 8;
-        break;
-      case "nine":
-        num = 9;
-        break;
-      default:
-        num = 0;
-        break;
-    }
-    if(equalResult)
+    if(equalResult === false)
     {
-      inputValue = "" + num;
-      equalResult = false;
+      equalResult = true;
+      exp = "" + eleName;
     }
     else {
-      if(inputValue==="0")
-      {
-        inputValue = "" + num;
-      }
-      else {
-        inputValue += num;
-      }
+      exp += eleName;
     }
-    document.getElementById("inputValue").value = inputValue;
+    document.getElementById("inputValue").value = exp;
 }
 
 function readChar(oper){
-    operation = oper;
-    firstOperand = parseInt(document.getElementById("inputValue").value);
-    document.getElementById("inputValue").value = "0";
+    exp += oper;
+    document.getElementById("inputValue").value = exp;
 }
